f11adce420a0d0a457fef545cb2c85b239e3b461
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2026-02-18 14:12:27 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+"""Module that queries the Reddit API and prints the top 10 hot posts."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a subreddit"""
+    """Prints the titles of the first 10 hot posts of a subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "alu-scripting-project/1.0"}
 
     try:
         res = requests.get(url, headers=headers, allow_redirects=False)
-
-        # Invalid subreddit or non-200 response
         if res.status_code != 200:
             print(None)
             return
