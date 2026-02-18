8e5e555fd876cc73a570afb51047d6716f662398
Nziza21 <n.samuel@alustudent.com>
2026-02-18 00:40:03 -0800

    top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""DOCS"""
 import requests
 
 
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    if not subreddit:
+        print("None")
+        return
 
-    if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
-    else:
-        print(None)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "alu-scripting-project/1.0"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code != 200:
+            print("None")
+            return
+
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+
+    except Exception:
+        print("None")
+
