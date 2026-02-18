f6785aabdb0dd4bf9b7a79d9aae1c018a1efc66f
Nziza21 <n.samuel@alustudent.com>
2026-02-18 01:22:07 -0800

    changed

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""DOCS"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'alu-scripting-project/1.0'}
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code != 200:
-            print(None)
-            return
-
-        posts = response.json().get('data', {}).get('children', [])
-
-        for post in posts:
-            print(post.get('data', {}).get('title'))
-
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
         print(None)
-
