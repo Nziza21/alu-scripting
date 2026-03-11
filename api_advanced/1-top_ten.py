5701bdce0e0ca61aa84876b1160ad1a91c8d1767
Nziza21 <n.samuel@alustudent.com>
2026-03-11 11:54:58 +0200

    Update User-Agent for Reddit API

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Print titles of first 10 hot posts for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "python:my.reddit.app:v1.0 (by /u/test)"}
+    headers = {"User-Agent": "linux:alu.project:v1.2 (by /u/alu_student)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
