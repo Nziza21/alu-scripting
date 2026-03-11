c4fc3321cfd380b49b0d23df1341b9eb7c38671f
Nziza21 <n.samuel@alustudent.com>
2026-03-11 11:58:17 +0200

    Simplify Reddit API top_ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,12 +8,9 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "linux:alu.project:v1.2 (by /u/alu_student)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    try:
+    if response.status_code == 200:
         data = response.json().get("data", {}).get("children", [])
         for post in data:
             print(post.get("data", {}).get("title"))
-    except Exception:
+    else:
         print(None)
